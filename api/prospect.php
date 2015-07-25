8b7c78fd72f0bee086f6ce2099062acb167979c0
Hugh Wells <wellshb@deanclose.org.uk>
2015-07-25 23:33:54 +0100

    changed not equal operator

--- api/prospect.php
+++ api/prospect.php
@@ -4,7 +4,7 @@
 
 	$user_id = $_GET["user_id"];
 
-	$query = "SELECT * FROM 'users' WHERE $user_id != 'user_id'";
+	$query = "SELECT * FROM 'users' WHERE $user_id <> 'user_id'";
 
 	$result = mysqli_query($con, $query); 
 	
