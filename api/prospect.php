d2d78fe2027fcbb52eee0e5e802e6b9a4edc499b
Hugh Wells <wellshb@deanclose.org.uk>
2015-07-26 00:45:14 +0100

    2 dimensional array

--- api/prospect.php
+++ api/prospect.php
@@ -2,13 +2,22 @@
 
 	require("../../config.php");
 
+	header('Content-Type: application/json');
+
 	$user_id = $_GET["user_id"];
 
 	$query = "SELECT * FROM `users` WHERE `user_id` <> $user_id";
 
-	$result = mysqli_query($con, $query); 
+	$result = mysqli_query($con, $query);
+
+	$data = []; 
+	$vari = 0;
 	
 	while ($row =mysqli_fetch_array($result)) {
-		print_r(array($row['name']));
+		$user[$vari] = [];
+		array_push($user, $row['user_id'], $row['name'], $row['dob'], $row['gender'], $row['interest'], $row['postcode'], $row['photo_url'], $row['range'], $row['min'], $row['max']);
+		
+		print_r($user[$vari]); 
+		$vari = $vari + 1; 
 	}
 ?>
