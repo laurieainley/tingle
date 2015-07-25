7ec06ad7f73b19eae9dfce178208fc5c495b2fae
Hugh Wells <wellshb@deanclose.org.uk>
2015-07-26 00:53:33 +0100

    Added recordings lookup

--- api/prospect.php
+++ api/prospect.php
@@ -15,9 +15,20 @@
 	
 	while ($row =mysqli_fetch_array($result)) {
 		$user[$vari] = [];
-		array_push($user[$vari], $row['user_id'], $row['name'], $row['dob'], $row['gender'], $row['interest'], $row['postcode'], $row['photo_url'], $row['range'], $row['min'], $row['max']);
+		array_push($user[$vari], $row['user_id'], $row['name'], $row['dob'], $row['gender'], $row['interest'], $row['postcode'], $row['photo_url'], $row['range'], $row['min'], $row['max'], $recordings[];);
 		
-		print_r($user[$vari]); 
+		
+
+		$query = "SELECT * FROM `recordings` WHERE `user_id` = $row['user_id'";
+		while ($row =mysqli_fetch_array($result)) {
+		$u_rec[$vari] = [];
+		array_push($u_rec[$vari], 'some stuff');
+
+		array_push($recordings, $u_rec);
+		}
+
+		array_push($data, $$user[$vari]);
+		print_r($data); 
 		$vari = $vari + 1; 
 	}
 ?>
