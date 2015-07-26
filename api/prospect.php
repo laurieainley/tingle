16d18711325570b1ca92c0bd78805415c648ce85
Hugh Wells <wellshb@deanclose.org.uk>
2015-07-26 12:06:17 +0100

    debug

--- api/prospect.php
+++ api/prospect.php
@@ -15,7 +15,7 @@
 
 	//first query 
 
-	$query = "SELECT * FROM `users` WHERE `user_id` <> $user_id AND `gender` = $interest";
+	$query = "SELECT * FROM `users` WHERE `user_id` <> $user_id AND `gender` = 'm'";
 
 	$result = mysqli_query($con, $query);
 	//set main array and variable for lopp and array names
