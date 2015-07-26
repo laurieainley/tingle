6e7739f3239fe0df14aff16878b917bf1c01677f
Hugh Wells <wellshb@deanclose.org.uk>
2015-07-26 10:30:33 +0100

    Asc array

--- api/prospect.php
+++ api/prospect.php
@@ -16,7 +16,7 @@
 	while ($row =mysqli_fetch_array($result)) {
 		$user[$vari] = [];
 		$recordings = [];
-		array_push($user[$vari], "user_id" => $row['user_id'], "name" => $row['name'], "dob" => $row['dob'], "gender" => $row['gender'], "interest" => $row['interest'], "postcode" => $row['postcode'], "photo_url" => $row['photo_url'], "range" => $row['range'], "min" => $row['min'], "max" => $row['max']);
+		$user[$vari] = array("user_id" => $row['user_id'], "name" => $row['name'], "dob" => $row['dob'], "gender" => $row['gender'], "interest" => $row['interest'], "postcode" => $row['postcode'], "photo_url" => $row['photo_url'], "range" => $row['range'], "min" => $row['min'], "max" => $row['max']);
 		
 		$cur_use = $row['user_id'];
 
