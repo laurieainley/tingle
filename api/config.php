1d89bd332f00d25cec48e719580b2ae6cfedaa00
Laurie Ainley <laurie@ainley.org>
2015-09-22 18:11:03 +0100

    DB name

--- api/config.php
+++ api/config.php
@@ -1,5 +1,5 @@
 $dbhost="mysql.tingledating.com";
 $dbusername="tingledating";
 $dbpassword="ukul3l3!";
-$dbname="tingle"; 
+$dbname="tingledating"; 
 $con = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname) or die("Kill me now");
