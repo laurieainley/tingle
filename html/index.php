85e0efee488845b5a83e78a13331d01788b92ecd
Laurie Ainley <laurie@ainley.org>
2015-09-24 12:08:17 +0100

    Fix for index

--- html/index.php
+++ html/index.php
@@ -4,11 +4,11 @@ include("header.php");
 ?>
 
 <br /><br /><br /><br /><br /><br /><br />
+<div style="text-align: center">
+  <p><a href="discover.php?uid=1000">Start listening to some Tingle ladies</a><br /><br /></p>
 
-<p><a href="discover.php?uid=1000">Start listening to some Tingle ladies</a><br /><br /></p>
-
-<p><a href="discover.php?uid=2000">Start listening to some Tingle guys</a></p>
-
+  <p><a href="discover.php?uid=2000">Start listening to some Tingle guys</a></p>
+</div>
 
 <?php
 include("footer.php");
