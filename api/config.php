9b03009329449224446986cfc3aba3461c038933
Laurie Ainley <laurie@ainley.org>
2015-09-24 01:40:31 +0100

    gitignore

--- api/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dbhost="mysql.tingledating.com";
-$dbusername="tingledating";
-$dbpassword="intrigu3d";
-$dbname="tingledating"; 
-$con = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname) or die("Kill me now");
---
+++ html/api/config.php
@@ -0,0 +1,6 @@
+<?php
+$dbhost="localhost";
+$dbusername="dbuser";
+$dbpassword="123";
+$dbname="dbname"; 
+$con = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname) or die("Kill me now");
