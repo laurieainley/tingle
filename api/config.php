e2fb9c70920ba9939290d2c1ebf59d0fac9e1695
Laurie Ainley <laurie@ainley.org>
2015-09-24 01:07:03 +0100

    Added Tingles

--- api/config.php
+++ api/config.php
@@ -1,6 +1,6 @@
 <?php
 $dbhost="mysql.tingledating.com";
 $dbusername="tingledating";
-$dbpassword="ukul3l3!";
+$dbpassword="intrigu3d";
 $dbname="tingledating"; 
 $con = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname) or die("Kill me now");
